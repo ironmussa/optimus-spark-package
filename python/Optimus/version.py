1a24ce5837fac7bf35129eaf2a4b5d7a82f7d849
faviovazquez <favio.vazquezp@gmail.com>
2017-10-02 18:54:22 -0500

    Update version

--- python/Optimus/version.py
+++
@@ -1,8 +0,0 @@
-def _safe_int(string):
-    try:
-        return int(string)
-    except ValueError:
-        return string
-
-__version__ = '1.0.0'
-VERSION = tuple(_safe_int(x) for x in __version__.split('.'))
