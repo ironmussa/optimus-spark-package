cabb3c7a79f5ae0b7a8de4750e678c3e5470166e
faviovazquez <favio.vazquezp@gmail.com>
2017-10-25 10:54:43 -0500

    Update Optimus

---
+++ python/tests/test_df_transformer.py
@@ -0,0 +1,90 @@
+from optimus.spark import get_spark
+import optimus as op
+from quinn.extensions import *
+from pyspark.sql.types import *
+
+class TestDataFrameTransformer(object):
+
+    def test_lower_case(self):
+        source_df = get_spark().create_df(
+            [
+                ("BOB", 1),
+                ("JoSe", 2)
+            ],
+            [
+                ("name", StringType(), True),
+                ("age", IntegerType(), False)
+            ]
+        )
+
+        transformer = op.DataFrameTransformer(source_df)
+        actual_df = transformer.lower_case("*").get_data_frame
+
+        expected_df = get_spark().create_df(
+            [
+                ("bob", 1),
+                ("jose", 2)
+            ],
+            [
+                ("name", StringType(), True),
+                ("age", IntegerType(), False)
+            ]
+        )
+
+        assert(expected_df.collect() == actual_df.collect())
+
+    def test_upper_case(self):
+        source_df = get_spark().create_df(
+            [
+                ("BOB", 1),
+                ("JoSe", 2)
+            ],
+            [
+                ("name", StringType(), True),
+                ("age", IntegerType(), False)
+            ]
+        )
+
+        transformer = op.DataFrameTransformer(source_df)
+        actual_df = transformer.upper_case("name").get_data_frame
+
+        expected_df = get_spark().create_df(
+            [
+                ("BOB", 1),
+                ("JOSE", 2)
+            ],
+            [
+                ("name", StringType(), True),
+                ("age", IntegerType(), False)
+            ]
+        )
+
+        assert(expected_df.collect() == actual_df.collect())
+
+    def test_trim_col(self):
+        source_df = get_spark().create_df(
+            [
+                ("  ron", 1),
+                ("      bill     ", 2)
+            ],
+            [
+                ("name", StringType(), True),
+                ("age", IntegerType(), False)
+            ]
+        )
+
+        transformer = op.DataFrameTransformer(source_df)
+        actual_df = transformer.trim_col("name").get_data_frame
+
+        expected_df = get_spark().create_df(
+            [
+                ("ron", 1),
+                ("bill", 2)
+            ],
+            [
+                ("name", StringType(), True),
+                ("age", IntegerType(), False)
+            ]
+        )
+
+        assert(expected_df.collect() == actual_df.collect())
